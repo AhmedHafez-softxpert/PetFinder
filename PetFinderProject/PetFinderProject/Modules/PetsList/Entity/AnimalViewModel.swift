a829ba5977aee4cf76bc76009da52a741a2bd6a3
AhmedHafez-softxpert <ahmed.hafez@softxpert.com>
2023-02-26 14:12:43 +0200

    edit AnimalViewModel initializer

--- PetFinderProject/PetFinderProject/Modules/PetsList/Entity/AnimalViewModel.swift
+++ PetFinderProject/PetFinderProject/Modules/PetsList/Entity/AnimalViewModel.swift
@@ -20,12 +20,49 @@ struct AnimalViewModel {
     var address: String
     var url: String
     
+    init(from animal: Animal) {
+        let firstSmallPhoto = (animal.photos?.count ?? 0) > 0 ? (animal.photos?[0].small ?? "") : ""
+        let firstMediumPhoto = (animal.photos?.count ?? 0) > 0 ? (animal.photos?[0].medium ?? "") : ""
+        let address = animal.contact?.address
+        let addressDetails = (address?.city ?? "") + (address?.state ?? "") + (address?.country ?? "")
+
+        self.name = animal.name ?? "NA"
+        self.size = animal.size ?? "NA"
+        self.type = animal.type ?? "NA"
+        self.gender = animal.gender ?? ""
+        self.firstSmallPhotoUrl = firstSmallPhoto
+        self.firstMediumPhotoUrl = firstMediumPhoto
+        self.primaryColor = animal.primaryColor ?? "NA"
+        self.address = addressDetails
+        self.url = animal.url ?? ""
+
+    }
+    
+   
+    
 }
+
+
 
 
 struct AnimalsViewModel {
     var animals: [AnimalViewModel]
     var nextPageUrl: String?
     
+    init?(from response: AnimalsResponse?) {
+        var petsArr: [AnimalViewModel] = []
+        print("handleAnimalResponse init arr \(petsArr)")
+        guard response != nil else {return nil}
+        let animals = response?.animals ?? []
+        for animal in animals {
+            var animalViewModel = AnimalViewModel(from: animal)
+            petsArr.append(animalViewModel)
+        }
+        self.nextPageUrl = response?.pagination?.nextPage
+        self.animals = petsArr
+    }
+    
     
 }
+
+
--- PetFinderProject/PetFinderProject/Modules/PetsList/Presenter/PetsListPresenter.swift
+++ PetFinderProject/PetFinderProject/Modules/PetsList/Presenter/PetsListPresenter.swift
@@ -23,38 +23,12 @@ class PetsListPresenter {
         interactor.getAnimals { [weak self] response in
             guard let self = self else {return}
             print("interactor.getAnimals from presenter")
-            let animalsViewModel = self.getAnimalsViewModel(from: response)
+            let animalsViewModel = AnimalsViewModel(from: response)
             self.view.updateDataSource(animalsViewModel: animalsViewModel)
             self.view.reloadData()
         }
     }
     
-    // should I move this func to struct AnimalsViewModel?
-    func getAnimalsViewModel(from response: AnimalsResponse?) -> AnimalsViewModel? {
-        var petsArr: [AnimalViewModel] = []
-        print("handleAnimalResponse init arr \(petsArr)")
-        guard response != nil else {return nil}
-        let animals = response?.animals ?? []
-        for animal in animals {
-            var animalViewModel = getAnimalViewModel(from: animal)
-            petsArr.append(animalViewModel)
-        }
-        let nextPageUrl = response?.pagination?.nextPage
-        let animalsViewModel = AnimalsViewModel(animals: petsArr, nextPageUrl: nextPageUrl)
-        return animalsViewModel
-    }
-    
-    
-    func getAnimalViewModel(from animal: Animal) -> AnimalViewModel {
-        let firstSmallPhoto = (animal.photos?.count ?? 0) > 0 ? (animal.photos?[0].small ?? "") : ""
-        let firstMediumPhoto = (animal.photos?.count ?? 0) > 0 ? (animal.photos?[0].medium ?? "") : ""
-        let address = animal.contact?.address
-        let addressDetails = (address?.city ?? "") + (address?.state ?? "") + (address?.country ?? "")
-        
-        let animalViewModel = AnimalViewModel(name: animal.name ?? "NA", size: animal.size ?? "NA", type: animal.type ?? "NA", gender: animal.gender ?? "", firstSmallPhotoUrl: firstSmallPhoto, firstMediumPhotoUrl: firstMediumPhoto, primaryColor: animal.primaryColor ?? "NA", address: addressDetails, url: animal.url ?? "")
-        
-        return animalViewModel
-    }
    
     
 }
