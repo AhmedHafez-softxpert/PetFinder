a64bfb0e0961c918f69588f2cc9e4c8d7121c26d
Ahmed Hafez <ahmed.hafez@softxpert.com>
2023-02-28 15:36:28 +0200

    add PetInfoCell

---
+++ PetFinderProject/PetFinderProject/CustomViews/PetInfoCell/PetInfoCell.swift
@@ -0,0 +1,43 @@
+//
+//  PetInfoCell.swift
+//  PetFinderProject
+//
+//  Created by SoftXpert on 2/28/23.
+//
+
+import UIKit
+
+class PetInfoCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var infoContentLabel: UILabel!
+    
+    @IBOutlet weak var infoTypeLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func configure(content: String) {
+        // content configuration  will be edidted later
+        configureInfoContentLabel(content: content)
+        configureInfoTypeLabel(type: content)
+    }
+
+}
+
+//MARK: - private methods
+
+extension PetInfoCell {
+    
+    private func configureInfoContentLabel(content: String) {
+        infoContentLabel.text = content
+    }
+    
+    private func configureInfoTypeLabel(type: String) {
+        infoTypeLabel.text = type
+    }
+    
+}
